326697c66fd25ae73a7ab268e0fb38c6953a8949
vovabatsyk <vovabatsyk@gmail.com>
2023-10-20 22:55:57 +0300

    cursor animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss";
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -9,6 +10,7 @@ import Services from "./components/services/Services";
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
@@ -23,7 +25,9 @@ const App = () => {
         <Parallax type="services" />
       </section>
       <Portfolio />
-      <section id="Contact"><Contact /></section>
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
